1976f11096e5c82eaff9882804a695892dbd5df9
Dimitar Dimitrov <daghostman.dd@gmail.com>
2017-03-27 13:32:26 +0300

    Fix switch application to be a delegate instead of middleware (common sense)

--- src/Framework/Application/Interfaces/ApplicationInterface.php
+++ src/Framework/Application/Interfaces/ApplicationInterface.php
@@ -1,14 +1,14 @@
 <?php declare(strict_types=1);
 namespace Onion\Framework\Application\Interfaces;
 
-use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
 
 /**
  * A special-case interface allowing to distinguish
  * between normal middleware and an application
  * instance
  */
-interface ApplicationInterface extends MiddlewareInterface
+interface ApplicationInterface extends DelegateInterface
 {
 
 }
