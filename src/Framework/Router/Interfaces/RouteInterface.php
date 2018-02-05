ea7cca6ca203559359314a8f73bfb6cceadb6251
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-02-05 23:04:55 +0200

    expand the methods of `RouteInterface`

--- src/Framework/Router/Interfaces/RouteInterface.php
+++ src/Framework/Router/Interfaces/RouteInterface.php
@@ -1,14 +1,39 @@
 <?php declare(strict_types=1);
 namespace Onion\Framework\Router\Interfaces;
 
-use Interop\Http\ServerMiddleware\DelegateInterface;
 use Onion\Framework\Hydrator\Interfaces\HydratableInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 interface RouteInterface extends HydratableInterface
 {
+    /** Get the readable name of the route */
     public function getName(): string;
+    /** Get a list of the supported methods */
     public function getMethods(): iterable;
+    /** Get the pattern of the route */
     public function getPattern(): string;
-    public function getDelegate(): DelegateInterface;
+    /** Get the request handler of the route middleware */
+    public function getRequestHandler(): RequestHandlerInterface;
+    /** Get the route parameters(if any) */
     public function getParameters(): iterable;
+
+    /** Check if the route has a name */
+    public function hasName(): bool;
+    /** Check if a route supports a specific method */
+    public function hasMethod(string $method): bool;
+
+    /**
+     * Add methods that are supported by the route
+     *
+     * This interface should be implemented in such a
+     * way to preserve immutability
+     *
+     * @return self
+     */
+    public function withMethods(iterable $methods): self;
+    /** Attach the request for the route */
+    public function withRequestHandler(RequestHandlerInterface $requestHandler): self;
+
+    /** Attempt to match the route against $path */
+    public function isMatch(string $path): bool;
 }
