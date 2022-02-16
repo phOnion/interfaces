f2989403d7d44068a7760d742d4ab32a0c0ec1d7
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-02-16 20:29:11 +0200

    update base client interface

--- src/Client/Interfaces/ClientInterface.php
+++ src/Client/Interfaces/ClientInterface.php
@@ -1,16 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Onion\Framework\Client\Interfaces;
 
-use Onion\Framework\Promise\Interfaces\PromiseInterface;
+use Onion\Framework\Loop\Interfaces\ResourceInterface;
 
 interface ClientInterface
 {
-    // public function on(string $event, callable $callback): void;
-    // public function proxy(StreamInterface $stream): void;
-    // public function connect(): PromiseInterface;
-    public function connect(string $address, int $port = null, int $timeout = 5): void;
-    public function send(string $data): string;
+    public static function send(
+        string $address,
+        string $data,
+        ?float $timeout = null,
+        array $contexts = [],
+    ): ResourceInterface;
 }
