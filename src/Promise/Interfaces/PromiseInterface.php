7764441c8e685a16fbead6f1599704e1eaa9062a
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-03-15 15:53:14 +0200

    remove non-compliant methods

--- src/Promise/Interfaces/PromiseInterface.php
+++ src/Promise/Interfaces/PromiseInterface.php
@@ -7,10 +7,5 @@ namespace Onion\Framework\Promise\Interfaces;
 interface PromiseInterface extends ThenableInterface
 {
     public function catch(callable $onRejected): static;
-    public function otherwise(callable $onRejected): static;
     public function finally(callable ...$final): static;
-
-    public function isPending(): bool;
-    public function isFulfilled(): bool;
-    public function isRejected(): bool;
 }
