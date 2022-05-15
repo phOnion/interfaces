079dfe2760047a5b839a97b664eb26d962d86da0
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-05-15 04:37:49 +0300

    add dependency registration methods to container

---
+++ src/Dependency/Interfaces/ContainerInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onion\Framework\Dependency\Interfaces;
+
+use Closure;
+use Onion\Framework\Dependency\Interfaces\FactoryInterface;
+use Psr\Container\ContainerInterface as PsrContainerInterface;
+use Onion\Framework\Dependency\Interfaces\ServiceProviderInterface;
+
+interface ContainerInterface extends PsrContainerInterface
+{
+    public function register(ServiceProviderInterface $provider): void;
+
+    public function singleton(string $id, string|Closure|FactoryInterface $binding): static;
+    public function bind(string $id, string|Closure|FactoryInterface $binding): static;
+
+    public function extend(string $className, Closure $decorator): static;
+}
