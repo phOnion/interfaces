5d2f79c61ff4632c829f9f1d00aa13c7d0a2da5e
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-05-20 02:59:05 +0300

    update route inerface

--- src/Router/Interfaces/RouteInterface.php
+++ src/Router/Interfaces/RouteInterface.php
@@ -4,23 +4,22 @@ declare(strict_types=1);
 
 namespace Onion\Framework\Router\Interfaces;
 
-use Psr\Http\Server\RequestHandlerInterface;
+use Closure;
+use Psr\Http\Server\MiddlewareInterface;
 
-interface RouteInterface extends RequestHandlerInterface
+interface RouteInterface
 {
     public function getName(): string;
     public function getMethods(): array;
     public function getPattern(): string;
-    public function getRequestHandler(): RequestHandlerInterface;
+    public function getAction(): Closure;
     public function getParameters(): array;
-    public function getHeaders(): array;
-    public function getParameter(string $name, $default = null);
+    public function getParameter(string $name, mixed $default = null);
 
     public function hasName(): bool;
     public function hasMethod(string $method): bool;
 
     public function withMethods(array $methods): self;
-    public function withRequestHandler(RequestHandlerInterface $requestHandler): self;
-    public function withHeaders(array $headers): self;
+    public function withAction(Closure|MiddlewareInterface $action): self;
     public function withParameters(array $parameters): self;
 }
