4063494807d7cee745febbbf110a4e466e8525af
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-05-25 00:51:46 +0300

    facilitate boot method inside an interface

---
+++ src/Dependency/Interfaces/BootableServiceProviderInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onion\Framework\Dependency\Interfaces;
+
+use Onion\Framework\Dependency\Interfaces\ServiceProviderInterface;
+use Onion\Framework\Dependency\Interfaces\ContainerInterface;
+
+interface BootableServiceProvider extends ServiceProviderInterface
+{
+    /**
+     * Handles late initializations of the service providers, that get
+     * triggered after all service providers' dependencies have been
+     * registered
+     */
+    public function boot(ContainerInterface $container): void;
+}
