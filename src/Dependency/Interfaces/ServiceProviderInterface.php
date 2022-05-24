6f0d7f92592f9d44abab60f364d4bee0989dda6d
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-05-25 00:50:53 +0300

    add minimal description to ServiceProviderInterface

--- src/Dependency/Interfaces/ServiceProviderInterface.php
+++ src/Dependency/Interfaces/ServiceProviderInterface.php
@@ -8,5 +8,12 @@ use Onion\Framework\Dependency\Interfaces\ContainerInterface;
 
 interface ServiceProviderInterface
 {
+    /**
+     * Facilitates the registration of dependencies with the container
+     * with which the provider is registered.
+     *
+     * @param ContainerInterface $provider With which the
+     * @return void
+     */
     public function register(ContainerInterface $provider): void;
 }
