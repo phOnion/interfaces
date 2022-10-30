38b309e32f575a5bb2dd23066f83ab78eb368a87
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-10-30 14:43:45 +0200

    define a constant for default supported protocols

--- src/Client/Interfaces/ClientInterface.php
+++ src/Client/Interfaces/ClientInterface.php
@@ -6,6 +6,8 @@ use Onion\Framework\Loop\Interfaces\ResourceInterface;
 
 interface ClientInterface
 {
+    const DEFAULT_STREAM_CRYPTO = STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT | STREAM_CRYPTO_METHOD_TLSv1_3_CLIENT;
+
     public static function setClientCryptoProtocol(int $protocol): void;
 
     public static function connect(
