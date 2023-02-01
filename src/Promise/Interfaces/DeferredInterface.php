98aa38a4fdcdeb68b2a7a3901871b1d4c58bce5f
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-02-01 20:58:47 +0200

    update include a method to check if a deferred has been resolved or not

--- src/Promise/Interfaces/DeferredInterface.php
+++ src/Promise/Interfaces/DeferredInterface.php
@@ -6,8 +6,33 @@ use Throwable;
 
 interface DeferredInterface
 {
+    /**
+     * Check if the deferred object has been resolved or is still
+     * pending its resolution
+     *
+     * @return bool
+     */
+    public function completed(): bool;
+
+    /**
+     * Retrieve the current deferred
+     * @return PromiseInterface
+     */
     public function promise(): PromiseInterface;
 
+    /**
+     * Resolve the current deferred object with the provided $value
+     *
+     * @param mixed $value
+     * @return void
+     */
     public function resolve(mixed $value): void;
+
+    /**
+     * Reject the current deferred object with the provided $exception
+     *
+     * @param Throwable $exception
+     * @return void
+     */
     public function reject(Throwable $exception): void;
 }
