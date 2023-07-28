923727adebf77f8930c37421925acf0b81cebcb8
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-07-29 01:29:58 +0300

    fix lock type parameter

--- src/Loop/Interfaces/MutexInterface.php
+++ src/Loop/Interfaces/MutexInterface.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 namespace Onion\Framework\Loop\Interfaces;
-use Onion\Framework\Loop\Types\LockType;
+use Onion\Framework\Loop\Types\Lock;
 
 interface MutexInterface
 {
-    public static function acquire(object $target, LockType $type = LockType::EXCLUSIVE): bool;
+    public static function acquire(object $target, Lock $type = Lock::EXCLUSIVE): bool;
     public static function release(object $target): bool;
 }
