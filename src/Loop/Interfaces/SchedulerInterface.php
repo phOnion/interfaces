608e8d047583a844851fb92dc0608de90779b1a8
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-08-04 00:07:28 +0300

    expand SchedulerInterface with networking methods

--- src/Loop/Interfaces/SchedulerInterface.php
+++ src/Loop/Interfaces/SchedulerInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Onion\Framework\Loop\Interfaces;
 
+use Onion\Framework\Client\Interfaces\ContextInterface as ClientContext;
+use Onion\Framework\Loop\Types\NetworkAddress;
+use Onion\Framework\Server\Interfaces\ContextInterface as ServerContext;
 use Onion\Framework\Loop\Interfaces\ResourceInterface;
+use Onion\Framework\Loop\Types\NetworkProtocol;
 
 /**
  * Represents an event loop
@@ -72,4 +76,56 @@ interface SchedulerInterface
      * Stops the event loop after completing the current tick
      */
     public function stop(): void;
+
+    /**
+     * Adds a new error handler to the loop. The handler will be
+     * executed when an error occurs during the execution of the loop
+     * and the error is not handled by the task itself.
+     */
+    public function addErrorHandler(Closure $handler): void;
+
+    /**
+     * Removes a previously added error handler from the loop.
+     */
+    public function removeErrorHandler(Closure $handler): void;
+
+    /**
+     * Open a $type server on the given $address and optionally $port
+     * using the given $protocol. The $callback will be executed when
+     * a new connection is established and the $context is used to
+     * configure the server.
+     *
+     * In the case where the $type is `NetworkAddress::LOCAL`, the
+     * $address parameter is used as the path to the socket to listen
+     * on and $port is ignored.
+     *
+     * @return string The address on which the server is listening
+     */
+    public function open(
+        string $address,
+        int $port,
+        Closure $callback,
+        NetworkProtocol $protocol = NetworkProtocols::TCP,
+        ?ServerContext $context = null,
+        NetworkAddress $type = NetworkAddress::NETWORK,
+    ): string;
+
+    /**
+     * Establish a $type connection to a designated $address and optionally $port
+     * using the given $protocol. The $callback will be executed when the connection
+     * is established and the $context is used to configure the connection.
+     *
+     * In the case where the $type is `NetworkAddress::LOCAL`, the $address parameter
+     * is used as the path to the socket to connect to and $port is ignored.
+     */
+    public function connect(
+        string $address,
+        int $port,
+        Closure $callback,
+        NetworkProtocol $protocol = NetworkProtocols::TCP,
+        ?ClientContext $context = null,
+        NetworkAddress $type = NetworkAddress::NETWORK,
+    ): void;
+
+
 }
