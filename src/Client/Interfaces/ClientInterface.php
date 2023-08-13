f4331d1537b8e7ad3afc925dce2dbfb15d65627b
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-08-13 14:50:10 +0300

    remove `send` method in favor of single connect method

--- src/Client/Interfaces/ClientInterface.php
+++ src/Client/Interfaces/ClientInterface.php
@@ -6,20 +6,8 @@ use Onion\Framework\Loop\Interfaces\ResourceInterface;
 
 interface ClientInterface
 {
-    const DEFAULT_STREAM_CRYPTO = STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT | STREAM_CRYPTO_METHOD_TLSv1_3_CLIENT;
-
-    public static function setClientCryptoProtocol(int $protocol): void;
-
     public static function connect(
         string $address,
-        ?float $timeout,
         ContextInterface ...$context,
-    ): ResourceInterface;
-
-    public static function send(
-        string $address,
-        string $data,
-        ?float $timeout = null,
-        array $contexts = [],
     ): ResourceInterface;
 }
