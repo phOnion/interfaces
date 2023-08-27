520f6ee91a8ddc9bf8b6b5599cf7608ac6c2d749
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-08-27 12:05:32 +0300

    switch to using callback instead of events for servers

--- src/Server/Interfaces/DriverInterface.php
+++ src/Server/Interfaces/DriverInterface.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 namespace Onion\Framework\Server\Interfaces;
+
+use Closure;
 use Psr\EventDispatcher\EventDispatcherInterface;
 
 interface DriverInterface
 {
-    public function listen(EventDispatcherInterface $dispatcher): void;
+    public function listen(EventDispatcherInterface $dispatcher, Closure $callback): void;
 }
--- src/Server/Interfaces/ServerInterface.php
+++ src/Server/Interfaces/ServerInterface.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Onion\Framework\Server\Interfaces;
 
+use Closure;
+
 interface ServerInterface
 {
-    public function attach(DriverInterface $driver): void;
+    public function attach(DriverInterface $driver, Closure $callback): void;
 
     public function start(): void;
 }
